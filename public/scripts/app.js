6fffb7068273934bb2a845f29cad586c30688431
teeaaspoon <tommypoon36@gmail.com>
2018-06-19 23:29:15 +0000

    rendered all tweets and added moment.js for time

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,26 +4,63 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const mockTweet = {
-    user: {
-        name: "Newton",
-        avatars: {
-            small:
-                "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-            regular:
-                "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-            large:
-                "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+const data = [
+    {
+        user: {
+            name: "Newton",
+            avatars: {
+                small:
+                    "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+                regular:
+                    "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+                large:
+                    "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+            },
+            handle: "@SirIsaac"
         },
-        handle: "@SirIsaac"
+        content: {
+            text:
+                "If I have seen further it is by standing on the shoulders of giants"
+        },
+        created_at: 1461116232227
     },
-    content: {
-        text:
-            "If I have seen further it is by standing on the shoulders of giants"
+    {
+        user: {
+            name: "Descartes",
+            avatars: {
+                small:
+                    "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+                regular:
+                    "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+                large:
+                    "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+            },
+            handle: "@rd"
+        },
+        content: {
+            text: "Je pense , donc je suis"
+        },
+        created_at: 1461113959088
     },
-    created_at: 1461116232227
-};
-
+    {
+        user: {
+            name: "Johann von Goethe",
+            avatars: {
+                small:
+                    "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+                regular:
+                    "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+                large:
+                    "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+            },
+            handle: "@johann49"
+        },
+        content: {
+            text: "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+        },
+        created_at: 1461113796368
+    }
+];
 function createTweetElement(tweetObject) {
     const fullName = tweetObject["user"]["name"];
     const profilePicture = tweetObject["user"]["avatars"]["large"];
@@ -31,7 +68,8 @@ function createTweetElement(tweetObject) {
     const tweetContent = tweetObject["content"]["text"];
     const postTime = tweetObject["created_at"];
 
-    return `<article class="tweet">
+    return `
+    <article class="tweet">
         <header>
           <img src="${profilePicture}" alt="" class="profile-picture">
           <h3 class="full-name">${fullName}</h3>
@@ -42,9 +80,7 @@ function createTweetElement(tweetObject) {
         <hr>
         <footer>
 
-          <span class="post-time">${new Date(postTime).toLocaleDateString(
-              "en-US"
-          )}</span>
+          <span class="post-time">${moment(postTime).fromNow()}</span>
           <div class="tweet-interact">
             <span class="fas fa-retweet fa-2x"></span>
             <span class="fas fa-heart fa-2x"></span>
@@ -52,9 +88,19 @@ function createTweetElement(tweetObject) {
           </div>
 
         </footer>
-      </article>`;
+        </article>`;
+}
+
+// new Date(postTime).toLocaleDateString(
+//     "en-US"
+// )
+
+function renderTweets(arrayOfTweetObjects) {
+    arrayOfTweetObjects.forEach(entry => {
+        $("#tweets-container").append(createTweetElement(entry));
+    });
 }
 
 $(document).ready(function() {
-    $("#tweets-container").append(createTweetElement(mockTweet));
+    renderTweets(data);
 });
