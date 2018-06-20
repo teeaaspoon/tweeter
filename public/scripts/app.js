db4590fd0e64022a5ad9f96d718b2f2c979e1c73
teeaaspoon <tommypoon36@gmail.com>
2018-06-20 17:10:54 +0000

    got tweet posting working

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,63 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-    {
-        user: {
-            name: "Newton",
-            avatars: {
-                small:
-                    "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-                regular:
-                    "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-                large:
-                    "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-            },
-            handle: "@SirIsaac"
-        },
-        content: {
-            text:
-                "If I have seen further it is by standing on the shoulders of giants"
-        },
-        created_at: 1461116232227
-    },
-    {
-        user: {
-            name: "Descartes",
-            avatars: {
-                small:
-                    "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-                regular:
-                    "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-                large:
-                    "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-            },
-            handle: "@rd"
-        },
-        content: {
-            text: "Je pense , donc je suis"
-        },
-        created_at: 1461113959088
-    },
-    {
-        user: {
-            name: "Johann von Goethe",
-            avatars: {
-                small:
-                    "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-                regular:
-                    "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-                large:
-                    "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-            },
-            handle: "@johann49"
-        },
-        content: {
-            text: "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-        },
-        created_at: 1461113796368
-    }
-];
 function createTweetElement(tweetObject) {
     const fullName = tweetObject["user"]["name"];
     const profilePicture = tweetObject["user"]["avatars"]["large"];
@@ -91,16 +34,39 @@ function createTweetElement(tweetObject) {
         </article>`;
 }
 
-// new Date(postTime).toLocaleDateString(
-//     "en-US"
-// )
-
 function renderTweets(arrayOfTweetObjects) {
     arrayOfTweetObjects.forEach(entry => {
-        $("#tweets-container").append(createTweetElement(entry));
+        $("#tweets-container").prepend(createTweetElement(entry));
+    });
+}
+
+function fetchTweets() {
+    $.ajax({
+        url: "/tweets",
+        type: "GET"
+    }).then(function(data) {
+        // clears all the tweets
+        $("#tweets-container").empty();
+        // re renders them
+        renderTweets(data);
     });
 }
 
 $(document).ready(function() {
-    renderTweets(data);
+    // render all the tweets first.
+    fetchTweets();
+
+    // listen for form submit
+    $("form").on("submit", function(event) {
+        event.preventDefault();
+        let tweetContent = $(event.target).serialize();
+        $.ajax({
+            url: "/tweets",
+            type: "POST",
+            data: tweetContent
+        }).then(function() {
+            //render all the tweets again
+            fetchTweets();
+        });
+    });
 });
