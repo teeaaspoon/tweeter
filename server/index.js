21868d40a2fc28ab98f927975568b8bb3bdb3d5f
teeaaspoon <tommypoon36@gmail.com>
2018-06-21 17:18:41 +0000

    modified getTweets to grab from MongoDB

--- server/index.js
+++ server/index.js
@@ -6,29 +6,43 @@ const PORT = 8080;
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const MongoClient = require("mongodb").MongoClient;
+const MONGODB_URI = "mongodb://localhost:27017/";
+
+// database name
+const dbName = "tweeter";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// Connect to Mongo
+MongoClient.connect(
+    MONGODB_URI,
+    (err, client) => {
+        console.log(`Connected successfully to: ${MONGODB_URI}`);
+        const db = client.db(dbName);
+
+        // The `data-helpers` module provides an interface to the database of tweets.
+        // This simple interface layer has a big benefit: we could switch out the
+        // actual database it uses and see little to no changes elsewhere in the code
+        // (hint hint).
+        //
+        // Because it exports a function that expects the `db` as a parameter, we can
+        // require it and pass the `db` parameter immediately:
+        const DataHelpers = require("./lib/data-helpers.js")(db);
+
+        // The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
+        // so it can define routes that use it to interact with the data layer.
+        const tweetsRoutes = require("./routes/tweets")(DataHelpers);
+
+        // Mount the tweets routes at the "/tweets" path prefix:
+        app.use("/tweets", tweetsRoutes);
+
+        app.listen(PORT, () => {
+            console.log("Example app listening on port " + PORT);
+        });
+    }
+);
+
 // The in-memory database of tweets. It's a basic object with an array in it.
 const db = require("./lib/in-memory-db");
-
-// The `data-helpers` module provides an interface to the database of tweets.
-// This simple interface layer has a big benefit: we could switch out the
-// actual database it uses and see little to no changes elsewhere in the code
-// (hint hint).
-//
-// Because it exports a function that expects the `db` as a parameter, we can
-// require it and pass the `db` parameter immediately:
-const DataHelpers = require("./lib/data-helpers.js")(db);
-
-// The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
-// so it can define routes that use it to interact with the data layer.
-const tweetsRoutes = require("./routes/tweets")(DataHelpers);
-
-// Mount the tweets routes at the "/tweets" path prefix:
-app.use("/tweets", tweetsRoutes);
-
-app.listen(PORT, () => {
-    console.log("Example app listening on port " + PORT);
-});
--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -5,23 +5,26 @@ const simulateDelay = require("./util/simulate-delay");
 
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
-  return {
+    const tweetsCollection = db.collection("tweets");
+    return {
+        // Saves a tweet to `db`
+        saveTweet: function(newTweet, callback) {
+            simulateDelay(() => {
+                db.tweets.push(newTweet);
+                callback(null, true);
+            });
+        },
 
-    // Saves a tweet to `db`
-    saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
-        callback(null, true);
-      });
-    },
-
-    // Get all tweets in `db`, sorted by newest first
-    getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
-      });
-    }
-
-  };
-}
+        // Get all tweets in `db`, sorted by newest first
+        getTweets: function(callback) {
+            simulateDelay(() => {
+                const sortNewestFirst = (a, b) => a.created_at - b.created_at;
+                // gets all the tweets from mongoDB
+                tweetsCollection.find({}).toArray((err, response) => {
+                    // sorts all the tweets in mongoDB
+                    callback(null, response.sort(sortNewestFirst));
+                });
+            });
+        }
+    };
+};
