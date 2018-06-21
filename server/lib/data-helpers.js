e1fb922aa14c5c904ff7cc2de3d292e0f8486159
teeaaspoon <tommypoon36@gmail.com>
2018-06-21 19:12:09 +0000

    removed simualate delay

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,29 +1,22 @@
 "use strict";
 
-// Simulates the kind of delay we see with network or filesystem operations
-const simulateDelay = require("./util/simulate-delay");
-
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
     const tweetsCollection = db.collection("tweets");
     return {
         // Saves a tweet to `db`
         saveTweet: function(newTweet, callback) {
-            simulateDelay(() => {
-                tweetsCollection.insertOne(newTweet);
-                callback(null, true);
-            });
+            tweetsCollection.insertOne(newTweet);
+            callback(null, true);
         },
 
         // Get all tweets in `db`, sorted by newest first
         getTweets: function(callback) {
-            simulateDelay(() => {
-                const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-                // gets all the tweets from mongoDB
-                tweetsCollection.find({}).toArray((err, response) => {
-                    // sorts all the tweets in mongoDB
-                    callback(null, response.sort(sortNewestFirst));
-                });
+            const sortNewestFirst = (a, b) => a.created_at - b.created_at;
+            // gets all the tweets from mongoDB
+            tweetsCollection.find({}).toArray((err, response) => {
+                // sorts all the tweets in mongoDB
+                callback(null, response.sort(sortNewestFirst));
             });
         }
     };
